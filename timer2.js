c9d66344af7d2378803d827af505a4332f7f96a2
nforne <mnforne@gmail.com>
2021-11-04 15:43:18 -0700

    add numbers capability and exit message to timer2.js

--- timer2.js
+++ timer2.js
@@ -8,10 +8,12 @@ stdin.setEncoding('utf8');
 
 // on any input from stdin (standard input), output a ... to stdout
 stdin.on('data', (key) => {
+  let numlist = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
   if (key === '\u0003') {
     // \u0003 maps to ctrl+c input
+    process.stdin.write('Thanks for using me, ciao!');
     process.exit();
-  } else if (key === 'b') {
+  } else if (key === 'b' || numlist.includes(key)) {
     process.stdout.write(key);
     process.stdout.write('\x07')
   }
